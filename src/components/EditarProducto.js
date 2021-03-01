a17dbec6a22a63a51e3e3c06aa402b1cdbf08cae
jorboare <jordi.b.arevalo@gmail.com>
2021-03-01 13:34:02 +0100

    Forms de Nuevo y Editar

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -2,7 +2,40 @@ import React from 'react';
 
 const EditarProducto = () => {
     return (
-        <h1>Desde Editar Producto</h1>
+        <div className='row justify-content-center'>
+            <div className='col-md-8'>
+                <div className='card'>
+                    <div className='card-body'>
+                        <h2 className='text-center mb-4 font-weight-bold'>Editar Producto</h2>
+
+                        <form>
+                            <div className='form-group'>
+                                <label>Nombre Producto</label>
+                                <input
+                                    type='text'
+                                    className='form-control'
+                                    placeholder='Nombre Producto'
+                                    name='nombre'
+                                />
+                            </div>
+                            <div>
+                                <label>Precio Producto</label>
+                                <input
+                                    type='number'
+                                    className='form-control'
+                                    placeholder='Precio Producto'
+                                    name='precio'
+                                />
+                            </div>
+                            <button
+                                type='submit'
+                                className='btn btn-primary font-weight-bold text-uppercase d-block w-100 mt-3'>Guardar Cambio</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 }
 

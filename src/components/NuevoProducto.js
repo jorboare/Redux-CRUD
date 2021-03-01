113890c70c83822f0ffbb4bbc08e18359d935717
jorboare <jordi.b.arevalo@gmail.com>
2021-03-01 13:18:53 +0100

    Initial config

---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NuevoProducto = () => {
+    return (
+        <div className='row justify-content-center'>
+            <div className='col-md-8'>
+                <div className='card'>
+                    <div className='card-body'>
+                        <h2 className='text-center mb-4 font-weight-bold'>Agregar Nuevo Producto</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NuevoProducto;
---
+++ src/components/Productos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Productos = () => {
+    return (
+        <p>Desde Productos</p>
+    );
+}
+
+export default Productos;

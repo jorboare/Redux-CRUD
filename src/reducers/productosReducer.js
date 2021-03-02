0cc690576cc351d430be858b7b169c477b9f2789
jorboare <jordi.b.arevalo@gmail.com>
2021-03-02 11:12:12 +0100

    Definir el action

--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -1,5 +1,10 @@
-//Cada reducer tiene su propio state
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
+} from './../types/index'
 
+//Cada reducer tiene su propio state
 const initialState = {
     productos: [],
     error: null,

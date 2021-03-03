349b9db956ce058a253784db97dd3d4181af9c07
jorboare <jordi.b.arevalo@gmail.com>
2021-03-03 11:42:58 +0100

    Delete button

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -1,7 +1,13 @@
 import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
-    AGREGAR_PRODUCTO_ERROR
+    AGREGAR_PRODUCTO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR,
+    OBTENER_PRODUCTO_ELIMINAR,
+    PRODUCTO_ELIMINADO_EXITO,
+    PRODUCTO_ELIMINADO_ERROR
 } from './../types/index'
 
 import clienteAxios from '../config/axios'
@@ -57,4 +63,70 @@ const agregarProductoExito = producto => ({
 const agregarProductoError = estado => ({
     type: AGREGAR_PRODUCTO_ERROR,
     payload: estado
+})
+
+// Función que descarga los productos de la base de datos
+export function obtenerProductosAction() {
+    return async (dispatch) => {
+        dispatch(descargarProductos())
+
+        try {
+            const respuesta = await clienteAxios.get('/productos')
+            dispatch(descargaProductosExitosa(respuesta.data))
+        } catch (error) {
+            console.log(error)
+            dispatch(descargaProductosError())
+        }
+    }
+}
+
+
+const descargarProductos = () => ({
+    type: COMENZAR_DESCARGA_PRODUCTOS,
+    payload: true
+})
+
+const descargaProductosExitosa = productos => ({
+    type: DESCARGA_PRODUCTOS_EXITO,
+    payload: productos
+})
+
+const descargaProductosError = () => ({
+    type: DESCARGA_PRODUCTOS_ERROR,
+    payload: true
+})
+
+//Selecciona y elimina el producto
+export function borrarProductoAction(id) {
+    return async (dispatch) => {
+        dispatch(obtenerProductoEliminar(id))
+        try {
+            await clienteAxios.delete(`/productos/${id}`)
+            dispatch(eliminarProductoExito())
+
+            // Si se elimina, mostrar alerta
+            Swal.fire(
+                'Eliminado!',
+                'El producto se eliminó correctamente.',
+                'success'
+            )
+        } catch (error) {
+            console.log(error)
+            dispatch(eliminarProductoError())
+        }
+    }
+}
+
+const obtenerProductoEliminar = id => ({
+    type: OBTENER_PRODUCTO_ELIMINAR,
+    payload: id
+})
+
+const eliminarProductoExito = () => ({
+    type: PRODUCTO_ELIMINADO_EXITO
+})
+
+const eliminarProductoError = () => ({
+    type: PRODUCTO_ELIMINADO_ERROR,
+    payload: true
 })
--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+//Redux
+import { useSelector, useDispatch } from 'react-redux'
+import { obtenerProductosAction } from '../actions/productoActions'
+
+//Components
+import Producto from './Producto'
 const Productos = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+
+        //Consultar la api
+        const cargarProductos = () => dispatch(obtenerProductosAction())
+        cargarProductos()
+    }, [])
+
+    //obtener el state
+    const productos = useSelector(state => state.productos.productos)
+    const error = useSelector(state => state.productos.error)
+    const cargando = useSelector(state => state.productos.loading)
+
     return (
         <>
             <h2 className='text-center my-5'>Listado de Productos</h2>
 
+            {error ? <p className='font-weight-bold alert alert-danger text-center mt-4'>Hubo un error</p> : null}
+            { cargando ? <p className='text-center'>Cargandp</p> : null}
             <table className='table table-stripped'>
                 <thead className='bg-primary table-dark'>
                     <tr>
@@ -14,7 +37,13 @@ const Productos = () => {
                     </tr>
                 </thead>
                 <tbody>
-
+                    {productos.length === 0 ? 'No hay productos' :
+                        (productos.map(producto => (
+                            <Producto
+                                key={producto.id}
+                                producto={producto}
+                            />
+                        )))}
                 </tbody>
             </table>
         </>
